e25435d05080639633a49a1198559d5a2e0080bf
mohammad nasiri <nasiri.mohammadreza92@yahoo.com>
2025-11-11 14:11:26 +0330

    debug

--- src/app/(main)/pricelist/[...slug]/layout.js
+++ src/app/(main)/pricelist/[...slug]/layout.js
@@ -17,7 +17,7 @@ export async function generateMetadata({ params }) {
  
   
 
-  if (!selectedCategory[0].parentTitle) {
+  if (!selectedCategory[0]?.parentTitle) {
     return {
       title: "محصولی موجود نیست",
       description: "محصولی در صفحه مورد نظر یافت نشد",
@@ -25,7 +25,7 @@ export async function generateMetadata({ params }) {
   }
 
   return {
-    title: `افرنگ | قیمت محصولات ${selectedCategory[0].parentTitle}`,
+    title: `افرنگ | قیمت محصولات ${selectedCategory[0]?.parentTitle}`,
   };
 }
 
@@ -38,6 +38,35 @@ export default async function PriceListLayout({ children, params }) {
      IsActive: 1,
     ParentIdArray: id,
   });
+  if (!selectedCategory || selectedCategory.length===0) {
+    return(
+      <>
+     <div className="flex flex-col items-center justify-center py-12">
+      <div className="w-24 h-24 !mb-4">
+        <svg
+          className="w-full h-full text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </div>
+      <h3 className="text-xl font-semibold text-gray-900 !mb-2">
+        دسته‌بندی یافت نشد
+      </h3>
+      <p className="text-gray-500 text-center">
+        در حال حاضر محصولی در این دسته‌بندی موجود نیست.
+      </p>
+    </div>
+      </>
+    )
+  }
   return (
     <>
       <div className="bg-white">
