42aae23a3f3fbbc7c377f8ea46add08e082f2f80
mohammad nasiri <nasiri.mohammadreza92@yahoo.com>
2025-11-19 17:47:27 +0330

    debug

--- src/components/Gallery/HeaderGallery.jsx
+++ src/components/Gallery/HeaderGallery.jsx
@@ -1,18 +1,11 @@
-"use client";
-
 import { getCategory } from "@/services/Category/categoryService";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
 import { FaTelegram } from "react-icons/fa6";
 import Container from "../container";
-import Loading from "../Loading";
 import SelectCat from "./SelectCat";
 import SelectSort from "./SelectSort";
 
 async function HeaderGallery() {
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
   let category = [];
 
   try {
@@ -42,12 +35,6 @@ async function HeaderGallery() {
             </div>
             <Link
               href={"/profile/Send-Photo"}
-              onClick={(ev) => {
-                ev.preventDefault();
-                startTransition(() => {
-                  router.push("/profile/Send-Photo");
-                });
-              }}
             >
               <div className="flex items-center rounded-sm bg-[#18d1be] !text-white px-3 py-3 cursor-pointer duration-300 hover:bg-[#40768c]">
                 <FaTelegram className="text-lg" />
@@ -59,7 +46,6 @@ async function HeaderGallery() {
           </div>
         </div>
       </Container>
-      {isPending && <Loading />}
     </>
   );
 }
